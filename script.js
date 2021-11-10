f62d87a1354390db4c8f48fa8b92933c0116f523
jpvdo <joao.pvo@aluno.ufop.edu.br>
2021-11-10 17:08:25 -0300

    Puxando mensagens do servidos e enviando manual

--- script.js
+++ script.js
@@ -3,25 +3,45 @@ let textoEnviado;
 const tiposMensagem = ['normal', 'reservada']
 const nomeUser = prompt('Digite o seu nome: ')
 
+const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages') 
+promessa.then(mensagensServidor)
 
+function mensagensServidor(resposta){
+    const tpMensagem = resposta.data
+    console.log(tpMensagem.length)
+    let caixaMensagens = document.querySelector('ul')
+
+    for(let i = 0; tpMensagem.length; i++){
+        const mensagem = tpMensagem[i]
+        // se a mensagem for do tipo status, fica com a cor padrao
+        if(mensagem.type === "status"){
+            const msgServidor = `<li class="msg-status">
+            <span class="hora">(${mensagem.time})</span>
+            <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
+            </li>`
+            caixaMensagens.innerHTML += msgServidor
+        } 
+        // se a mensagem for do tipo message, adiciona a class normnal
+        if(mensagem.type === "message"){
+            const msgServidor = `<li class="msg-status normal">
+            <span class="hora">(${mensagem.time})</span>
+            <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
+            </li>`
+            caixaMensagens.innerHTML += msgServidor
+        }
+    }
+}
 
 function enviarMensagem(){
     // Ao clicar no icon o usuario vai armazenar o texto em uma variavel
     input = document.querySelector('.caixa-texto')
     textoEnviado = input.value
-    console.log(input.value)
-
-    const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages') 
-    promessa.then(editarMensagem())
-
+    let caixaMensagens = document.querySelector('ul')
+    const msgEnviada = `<li class="msg-status normal">
+    <span class="hora">(03:12:45)</span>
+    <span class="msg"><strong>${nomeUser+" "}</strong></span>
+    <span>${textoEnviado}</span>
+    </li>`
+    caixaMensagens.innerHTML += msgEnviada
     input.value="Escreva aqui..."
-}
-
-function editarMensagem(resposta){
-    let caixaMensagens = document.querySelector('nav')
-    const msgEnviada = `<div class="msg-status">
-    <p class="hora">(03:12:45)</p>
-    <p class="msg"><span>${nomeUser+" "}</span>${textoEnviado}</p>
-    </div>`
-    caixaMensagens.innerHTML = msgEnviada + caixaMensagens.innerHTML
 }
