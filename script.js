176c660e6f80026373ab3038599c1f680f4d5a0f
jpvdo <joao.pvo@aluno.ufop.edu.br>
2021-11-13 01:36:44 -0300

    Tela de login feita, e bug de digitar corrigido

--- script.js
+++ script.js
@@ -1,78 +1,105 @@
 const input = document.querySelector('.caixa-texto')
-let textoEnviado;
+let textoEnviado
+let name 
+let dataTempo
 // entrar na sala
-const name = prompt('Digite o seu nome: ')
-const promessaUser = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name})
-promessaUser.then(entradaUser)
-promessaUser.catch(reloadUser)
-
+function inicioSite() {
+    name = document.querySelector('.caixa-texto-user').value
+    console.log(name)
+    const promessaUser = axios.post(
+      'https://mock-api.driven.com.br/api/v4/uol/participants',
+      { name }
+    )
+    promessaUser.then(entradaUser)
+    promessaUser.catch(reloadUser)
+}
 
 // enviar mensagem
 let mensagemEnviada = {}
 
-console.log(mensagemEnviada)
-function enviarClique(){
-    mensagemEnviada.text= input.value
-    mensagemEnviada.type="message"
-    mensagemEnviada.from=name
-    mensagemEnviada.to="Todos"
-    console.log("enviando clique")
-    const promessaMsg = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', mensagemEnviada) 
-    input.value=""
+function enviarClique() {
+  mensagemEnviada.text = input.value
+  mensagemEnviada.type = 'message'
+  mensagemEnviada.from = name
+  mensagemEnviada.to = 'Todos'
+  const promessaMsg = axios.post(
+    'https://mock-api.driven.com.br/api/v4/uol/messages',
+    mensagemEnviada
+  )
+  input.value = ''
+  recarregarMsg()
 }
 
 // function: entrar na sala
-function entradaUser(resposta){
-    const validaUser = resposta.status
-    console.log(validaUser)
-    recarregarMsg()
-    
+function entradaUser(resposta) {
+  const removeFundo = document.querySelector('.area-login')
+  removeFundo.classList.add('display-none')
+  
+  const validaUser = resposta.status
+  console.log(validaUser)
+  setInterval(manterOnline, 5000)
+  recarregarMsg()
+  setInterval(recarregarMsg, 3000)
 }
-function reloadUser(resposta){
-        location.reload()
+function reloadUser(resposta) {
+  location.reload()
 }
 
 // manter user online de 5 em 5 segundos
-setInterval(manterOnline,5000)
-function manterOnline(){
-const promessaUserOn = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name}) 
-console.log('online')
+
+function manterOnline() {
+  const promessaUserOn = axios.post(
+    'https://mock-api.driven.com.br/api/v4/uol/status',
+    { name }
+  )
+  console.log('online')
 }
 
 // function: recarregar de 3 em 3 segundos
-setInterval(recarregarMsg, 3000)
-function recarregarMsg (){
-    // carregar mensagens do servidor
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages') 
-    promessa.then(mensagensServidor)
+
+function recarregarMsg() {
+  // carregar mensagens do servidor
+  const promessa = axios.get(
+    'https://mock-api.driven.com.br/api/v4/uol/messages'
+  )
+  promessa.then(mensagensServidor)
 }
 
-
 // function: carregar mensagens do servidor
-function mensagensServidor(resposta){
-    const tpMensagem = resposta.data
-    console.log(tpMensagem.length)
-    let caixaMensagens = document.querySelector('ul')
-
-    for(let i = 0; i<tpMensagem.length; i++){
-        let mensagem = tpMensagem[i]
-        // se a mensagem for do tipo status, fica com a cor padrao
-        if(mensagem.type === "status"){
-            const msgServidor = `<li class="msg-status">
-            <span class="hora">(${mensagem.time})</span>
-            <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
-            </li>`
-            caixaMensagens.innerHTML += msgServidor
-        } 
-        // se a mensagem for do tipo message, adiciona a class normnal
-        if(mensagem.type === "message"){
-            const msgServidor = `<li class="msg-status normal">
-            <span class="hora">(${mensagem.time})</span>
-            <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
-            </li>`
-            caixaMensagens.innerHTML += msgServidor
+function mensagensServidor(resposta) {
+  console.log("Lendo msg")
+  const tpMensagem = resposta.data
+  if (dataTempo!==tpMensagem[tpMensagem.length-1].time) {
+      console.log('FEURR')
+      let caixaMensagens = document.querySelector('ul')
+      let itens = document.querySelectorAll('li')
+      if (itens.length>0){
+          caixaMensagens.innerHTML = ''
         }
-        let elementoQueQueroQueApareca = caixaMensagens.querySelector('li:last-child');
-        elementoQueQueroQueApareca.scrollIntoView();
+        let todasAsMensagens = ''
+        for (let i = 0; i < tpMensagem.length; i++) {
+            let mensagem = tpMensagem[i]
+    // se a mensagem for do tipo status, fica com a cor padrao
+    if (mensagem.type === 'status') {
+        caixaMensagens.innerHTML += `<li class="msg-status">
+        <span class="hora">(${mensagem.time})</span>
+        <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
+        </li>`
+        //   todasAsMensagens += msgServidor
     }
+    // se a mensagem for do tipo message, adiciona a class normnal
+    if (mensagem.type === 'message') {
+        caixaMensagens.innerHTML += `<li class="msg-status normal">
+            <span class="hora">(${mensagem.time})</span>
+            <span class="msg"><strong>${mensagem.from} </strong>${mensagem.text}</span>
+            </li>`
+    //   todasAsMensagens += msgServidor
+}
+}
+dataTempo=tpMensagem[tpMensagem.length-1].time
+console.log("carregou")
+//   caixaMensagens.innerHTML += todasAsMensagens
+let elementoQueQueroQueApareca = caixaMensagens.querySelector('li:last-child')
+elementoQueQueroQueApareca.scrollIntoView()
+}
 }
